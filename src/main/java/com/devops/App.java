2b3843fbdcc5c2ba5bf8a68a0a06d0f103bd238f
akash-devops2 <akashpcloud9072@gmail.com>
2025-06-27 23:40:38 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,9 +1,12 @@
 package com.devops;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from DevOps Java App!");
-        System.out.println("New change from feature-branch-1-hello from branch ");
+import java.util.logging.Logger;
 
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Hello from DevOps Java App!");
+        logger.info("New change from feature-branch-1 - hello from branch");
     }
 }
