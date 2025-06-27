61e07ec5ea22b4f07c09860ecb7f840334082b24
akash-devops2 <akashpcloud9072@gmail.com>
2025-06-27 23:23:52 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -3,5 +3,7 @@ package com.devops;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Java App!");
+        System.out.println("New change from feature-branch-1-hello from branch ");
+
     }
 }
